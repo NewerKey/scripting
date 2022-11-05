c77b01d34b58781fa82ed51051c3c63911f420c8
Yvette Nartey <newerkey@gmail.com>
2022-11-05 10:19:34 +0100

    feat(filename): loop through images to convert

--- image_processing/jpeg_to_png_converter.py
+++ image_processing/jpeg_to_png_converter.py
@@ -9,3 +9,8 @@ output_folder = sys.argv[2]
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    img.save(f'{output_folder}{filename}', 'png')
+    print('done!')
